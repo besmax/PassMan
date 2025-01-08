a5b99e19e0d8d3eaeac8ced410a7810ac51ff248
Max <bespalov.m.9@ya.ru>
2025-01-08 15:01:52 +0300

    add some widgets to settings screen

--- features/main/src/main/java/bes/max/features/main/ui/SettingsScreen.kt
+++ features/main/src/main/java/bes/max/features/main/ui/SettingsScreen.kt
@@ -1,10 +1,91 @@
 package bes.max.features.main.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import bes.max.features.main.ui.icon.importIcon
+import bes.max.passman.features.main.R
+import bes.max.ui.common.ShowTitle
 
 @Composable
 fun SettingsScreen(
     navigateBack: () -> Unit,
 ) {
 
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+        ShowTitle(
+            title = stringResource(R.string.settings),
+            goBack = navigateBack,
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        SettingsItem(
+            text = stringResource(R.string.settings_item_import),
+            onItemClick = {},
+            icon = importIcon,
+            contentDescription = stringResource(R.string.settings_item_import_descr),
+        )
+    }
+}
+
+@Composable
+private fun SettingsItem(
+    text: String,
+    onItemClick: () -> Unit,
+    icon: ImageVector,
+    contentDescription: String? = null,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+            .clickable { onItemClick() }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = contentDescription,
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = text
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.KeyboardArrowRight,
+                contentDescription = contentDescription,
+                modifier = Modifier,
+            )
+        }
+
+    }
 }
