117e2538dbd2ccadfa9eb13414f56da1b0087eba
bespalovm <bespalovm@radioavionica.ru>
2025-02-11 15:55:14 +0300

    add background to settings screen

--- features/main/src/main/java/bes/max/features/main/ui/SettingsScreen.kt
+++ features/main/src/main/java/bes/max/features/main/ui/SettingsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,7 +23,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
@@ -41,9 +41,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
@@ -98,10 +102,24 @@ fun SettingsScreen(
 
     val scrollState = rememberScrollState()
 
+    val imageVector: ImageVector = ImageVector.vectorResource(R.drawable.icon_background)
+    val vectorPainter = rememberVectorPainter(image = imageVector)
+
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .verticalScroll(scrollState),
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+            .drawWithContent {
+                with(vectorPainter) {
+                    val size = this@drawWithContent.size.copy(
+                        width = this@drawWithContent.size.width * 1.1f,
+                        height = this@drawWithContent.size.height * 1.1f
+                    )
+                    this@drawWithContent.draw(size = size)
+                }
+
+                drawContent()
+            },
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
